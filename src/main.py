7f5cfe1c1497d537b9009d91f934df8ebcc7d417
ysy970923 <ysy970923@gmail.com>
2021-01-07 13:58:55 +0900

    change notations

--- src/main.py
+++ src/main.py
@@ -1,12 +1,3 @@
-import argparse
-
-import torch
-import torchvision
-from torchvision import transforms
-import torch.nn as nn
-import numpy as np
-
-import customModules as cm
 import parameterServer as ps
 import participant as pt
 import dataLoader as dl
@@ -19,11 +10,14 @@ if __name__ == "__main__":
     parser.add_argument('--n_participants', type=int, default=90, help='')
     parser.add_argument('--batch_size', type=int, default=1, help='')
     parser.add_argument('--theta_u', type=float, default=0.1, help='')
-    parser.add_argument('--local_data_ratio', type=float, default=0.01, help='')
-    parser.add_argument('--n_server_update', type=int, default=5, help='')
+    parser.add_argument('--local_data_ratio', type=float, default=0.01, help='data size ratio each participant has')
+    parser.add_argument('--n_server_update_single_interval', type=int, default=5, help='')
     parser.add_argument('--n_participants_single_interval', type=int, default=10, help='')
-    parser.add_argument('--interval', type=int, default=10, help='')
-
+    parser.add_argument('--n_intervals', type=int, default=200, help='')
+    # 개념적으로는 server_update가 동일한 시간마다 일어난다고 가정
+    # interval은 lagging에 한계를 주기 위해서 maximum lagging이 일어날 수 있는 시간 간격
+    # steps single interval이 많을 수록 interval 내에서 download와 upload 횟수가 늘어난다
+    # n_participants_single_interval이 클 수록 interval 별로 participant 수가 적어져서 일정 수준의 upload를 보장한다.
     '''
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
@@ -54,11 +48,11 @@ if __name__ == "__main__":
     for i in range(opt.n_participants):
         participants.append(pt.Participant('device-' + str(i), cm.SimpleCNN, server, paramsNum, opt.theta_u, train_loader[i]))
 
-    for _ in range(opt.interval):
+    for _ in range(opt.n_intervals):
         participants_ready = [False for _ in range(opt.n_participants)]
         participants_this_interval = np.random.permutation(np.arange(opt.n_participants))[:opt.n_participants_single_interval]
 
-        server_update = sorted(np.random.permutation(np.arange(opt.n_steps_single_interval))[:opt.n_server_update - 1])
+        server_update = sorted(np.random.permutation(np.arange(opt.n_steps_single_interval))[:opt.n_server_update_single_interval - 1])
         server_update_num = 0
 
         for step in range(opt.n_steps_single_interval):
@@ -71,7 +65,7 @@ if __name__ == "__main__":
                 participants[participant_num].download_params()
                 participants_ready[participant_num] = True
 
-            if server_update_num == opt.n_server_update - 1:
+            if server_update_num == opt.n_server_update_single_interval - 1:
                 continue
             if server_update[server_update_num] == step:
                 server.update()
